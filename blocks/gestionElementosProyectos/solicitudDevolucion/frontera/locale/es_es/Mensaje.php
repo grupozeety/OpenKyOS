133b6ce9136bef3666b3095ec70db638a62bd089
stiv15 <stivmarquez.ud@gmail.com>
2016-08-02 20:53:49 -0500

    Desarrollo Parcial Solicitud Devolución (Adicion Campo Proyecto)

--- blocks/gestionElementosProyectos/solicitudDevolucion/frontera/locale/es_es/Mensaje.php
+++ blocks/gestionElementosProyectos/solicitudDevolucion/frontera/locale/es_es/Mensaje.php
@@ -2,7 +2,15 @@
 /**
  * Etiquetas correspondientes a los Formularios
  */
- 
+
+$this->idioma ['solicitudDevolucion'] = 'Solicitud Devolución Material';
+
+
+
+$this->idioma ['proyecto'] = 'Proyecto :';
+$this->idioma ['proyectoTitulo'] = 'Ingrese Nombre del Proyecto';
+
+
 $this->idioma ['noDefinido'] = 'Etiqueta No definida';
 
 ?>
--- blocks/gestionElementosProyectos/solicitudDevolucion/frontera/registroSolicitud.php
+++ blocks/gestionElementosProyectos/solicitudDevolucion/frontera/registroSolicitud.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace gestionElementosProyectos\solicitudDevolucion\frontera;
+
 /**
  * IMPORTANTE: Este formulario está utilizando jquery.
  * Por tanto en el archivo ready.php se declaran algunas funciones js
@@ -20,20 +21,23 @@ class Registrador {
 		$this->miFormulario = $formulario;
 	}
 	function mostrarFormulario() {
-		var_dump ( $_REQUEST );
+		
 		// Rescatar los datos de este bloque
 		$esteBloque = $this->miConfigurador->getVariableConfiguracion ( "esteBloque" );
 		
 		// ---------------- SECCION: Parámetros Globales del Formulario ----------------------------------
 		
-		// -------------------------------------------------------------------------------------------------
+		$atributosGlobales ['campoSeguro'] = 'true';
+		
+		$_REQUEST ['tiempo'] = time ();
 		
 		// ---------------- SECCION: Parámetros Generales del Formulario ----------------------------------
 		$esteCampo = $esteBloque ['nombre'];
+		
 		$atributos ['id'] = $esteCampo;
 		$atributos ['nombre'] = $esteCampo;
 		// Si no se coloca, entonces toma el valor predeterminado 'application/x-www-form-urlencoded'
-		$atributos ['tipoFormulario'] = '';
+		$atributos ['tipoFormulario'] = 'multipart/form-data';
 		// Si no se coloca, entonces toma el valor predeterminado 'POST'
 		$atributos ['metodo'] = 'POST';
 		// Si no se coloca, entonces toma el valor predeterminado 'index.php' (Recomendado)
@@ -49,20 +53,100 @@ class Registrador {
 		$atributos ['tipoEtiqueta'] = 'inicio';
 		echo $this->miFormulario->formulario ( $atributos );
 		{
-		
-		/**
-		 * Código Formulario
-		 */
 			
+			/**
+			 * Código Formulario
+			 */
 			
+			$esteCampo = 'ficheros';
+			$atributos ['id'] = $esteCampo;
+			$atributos ['leyenda'] = "Solicitud Elementos No Consumidos";
+			echo $this->miFormulario->agrupacion ( 'inicio', $atributos );
+			unset ( $atributos );
+			{
+				
+				$esteCampo = "proyecto";
+				$atributos ['id'] = $esteCampo;
+				$atributos ['nombre'] = $esteCampo;
+				$atributos ['tipo'] = 'text';
+				$atributos ['estilo'] = 'jqueryui';
+				$atributos ['marco'] = true;
+				$atributos ['estiloMarco'] = '';
+				$atributos ["etiquetaObligatorio"] = true;
+				$atributos ['columnas'] = 1;
+				$atributos ['dobleLinea'] = 0;
+				$atributos ['tabIndex'] = $tab;
+				$atributos ['etiqueta'] = $this->lenguaje->getCadena ( $esteCampo );
+				$atributos ['validar'] = '';
+				$atributos ['textoFondo'] = 'Ingrese Nombre Proyecto';
+				if (isset ( $_REQUEST [$esteCampo] )) {
+					$atributos ['valor'] = $_REQUEST [$esteCampo];
+				} else {
+					$atributos ['valor'] = '';
+				}
+				$atributos ['titulo'] = $this->lenguaje->getCadena ( $esteCampo . 'Titulo' );
+				$atributos ['deshabilitado'] = false;
+				$atributos ['tamanno'] = 60;
+				$atributos ['maximoTamanno'] = '';
+				$atributos ['anchoEtiqueta'] = 100;
+				$tab ++;
+				
+				// Aplica atributos globales al control
+				$atributos = array_merge ( $atributos, $atributosGlobales );
+				echo $this->miFormulario->campoCuadroTexto ( $atributos );
+				unset ( $atributos );
+				
+				$esteCampo = 'id_proyecto';
+				$atributos ["id"] = $esteCampo; // No cambiar este nombre
+				$atributos ["tipo"] = "hidden";
+				$atributos ['estilo'] = '';
+				$atributos ["obligatorio"] = false;
+				$atributos ['marco'] = true;
+				$atributos ["etiqueta"] = "";
+				if (isset ( $_REQUEST [$esteCampo] )) {
+					$atributos ['valor'] = $_REQUEST [$esteCampo];
+				} else {
+					$atributos ['valor'] = '';
+				}
+				$atributos = array_merge ( $atributos, $atributosGlobales );
+				echo $this->miFormulario->campoCuadroTexto ( $atributos );
+				unset ( $atributos );
+			}
+			echo $this->miFormulario->agrupacion ( 'fin' );
+			unset ( $atributos );
 			
+			$valorCodificado = "actionBloque=" . $esteBloque ["nombre"];
+			$valorCodificado .= "&pagina=" . $this->miConfigurador->getVariableConfiguracion ( 'pagina' );
+			$valorCodificado .= "&bloque=" . $esteBloque ['nombre'];
+			$valorCodificado .= "&bloqueGrupo=" . $esteBloque ["grupo"];
+			$valorCodificado .= "&opcion= ";
 			
+			/**
+			 * SARA permite que los nombres de los campos sean dinámicos.
+			 * Para ello utiliza la hora en que es creado el formulario para
+			 * codificar el nombre de cada campo. Si se utiliza esta técnica es necesario pasar dicho tiempo como una variable:
+			 * (a) invocando a la variable $_REQUEST ['tiempo'] que se ha declarado en ready.php o
+			 * (b) asociando el tiempo en que se está creando el formulario
+			 */
+			$valorCodificado .= "&campoSeguro=" . $_REQUEST ['tiempo'];
+			$valorCodificado .= "&tiempo=" . time ();
+			// Paso 2: codificar la cadena resultante
+			$valorCodificado = $this->miConfigurador->fabricaConexiones->crypto->codificar ( $valorCodificado );
 			
-			
+			$atributos ["id"] = "formSaraData"; // No cambiar este nombre
+			$atributos ["tipo"] = "hidden";
+			$atributos ['estilo'] = '';
+			$atributos ["obligatorio"] = false;
+			$atributos ['marco'] = true;
+			$atributos ["etiqueta"] = "";
+			$atributos ["valor"] = $valorCodificado;
+			echo $this->miFormulario->campoCuadroTexto ( $atributos );
+			unset ( $atributos );
 		}
 		
 		// ----------------FINALIZAR EL FORMULARIO ----------------------------------------------------------
 		// Se debe declarar el mismo atributo de marco con que se inició el formulario.
+		
 		$atributos ['marco'] = true;
 		$atributos ['tipoEtiqueta'] = 'fin';
 		echo $this->miFormulario->formulario ( $atributos );
@@ -102,10 +186,10 @@ class Registrador {
 	}
 }
 
-$miSeleccionador = new Registrador( $this->lenguaje, $this->miFormulario );
+$miSeleccionador = new Registrador ( $this->lenguaje, $this->miFormulario );
 
 $miSeleccionador->mensaje ();
 
-$miSeleccionador->mostrarFormulario();
+$miSeleccionador->mostrarFormulario ();
 
 ?>
