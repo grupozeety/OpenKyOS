cc0b7a4fb4e5dd06825e466d9a6d555768652d16
emmanueltaborda2015 <tabordaemmanuel@gmail.com>
2016-08-05 16:53:38 -0500

    commit caso de uso asignar materiales a actividad.

---
+++ blocks/asignarMateriales/css/Estilo.php
@@ -0,0 +1,28 @@
+<?php
+if(!isset($GLOBALS["autorizado"])) {
+	include("../index.php");
+	exit;
+}
+
+$indice=0;
+$estilo[$indice++]="bootstrap-table.min.css";
+$estilo[$indice++]="bootstrap.min.css";
+$estilo[$indice++]="select2.min.css";
+$estilo[$indice++]="miestilo.css";
+
+
+
+$rutaBloque=$this->miConfigurador->getVariableConfiguracion("host");
+$rutaBloque.=$this->miConfigurador->getVariableConfiguracion("site");
+
+if($unBloque["grupo"]==""){
+	$rutaBloque.="/blocks/".$unBloque["nombre"];
+}else{
+	$rutaBloque.="/blocks/".$unBloque["grupo"]."/".$unBloque["nombre"];
+}
+
+foreach ($estilo as $nombre){
+	echo "<link rel='stylesheet' type='text/css' href='".$rutaBloque."/css/".$nombre."'>\n";
+
+}
+?>
---
+++ blocks/asignarMateriales/script/ready.js
@@ -0,0 +1,38 @@
+$("#<?php echo $this->campoSeguro('proyecto')?>").select2({width:'100%'});
+$("#<?php echo $this->campoSeguro('actividad')?>").select2({width:'100%'});
+$("#<?php echo $this->campoSeguro('identBeneficiario')?>").select2({width:'100%'});
+$("#<?php echo $this->campoSeguro('material')?>").select2({width:'100%'});
+$("#<?php echo $this->campoSeguro('unidad')?>").select2({width:'100%'});
+
+var i=1;
+
+$("#<?php echo $this->campoSeguro('botonAgregar')?>").click(function(){
+
+	$("#addr0").html('');
+	
+	var material = $("#<?php echo $this->campoSeguro('material')?> option:selected").text();
+	var unidad = $("#<?php echo $this->campoSeguro('unidad')?> option:selected").text();
+	var cantidad = $("#<?php echo $this->campoSeguro('cantidad')?>").val();
+	
+	$('#addr'+ i).html("<td>" + "<input type='checkbox' id='checkbox"+(i)+"'>" +"</td><td><input type='hidden' name='nombre' value='" + material + "'>"+ material +"</td><td>"+unidad+"</td><td>"+cantidad+"</td>");
+	$('#tabla1').append('<tr id="addr'+(i+1)+'"></tr>');
+ 
+	$("#<?php echo $this->campoSeguro('material')?>").val(''); 
+	$("#<?php echo $this->campoSeguro('material')?>").change();
+	$("#<?php echo $this->campoSeguro('unidad')?>").val('');
+	$("#<?php echo $this->campoSeguro('unidad')?>").change();
+	$("#<?php echo $this->campoSeguro('cantidad')?>").val("1");
+	
+	$('#myModal').modal('hide');
+	
+	i++;
+ 
+});
+
+$("#remove").click(function(){
+    for(j=0; j<i; j++){
+    	if( $('#checkbox' + (j)).prop('checked') ) {
+    		$("#addr"+(j)).remove();
+    	}
+    }
+});
