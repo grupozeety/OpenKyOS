8cee6d4dfa9909f436542b9d76106dde2aaa21e1
EmmanuelTaborda2015 <tabordaemmanuel@gmail.com>
2016-08-31 09:19:44 -0500

    Update procesarAjax.php

--- blocks/consumoMateriales/funcion/procesarAjax.php
+++ blocks/consumoMateriales/funcion/procesarAjax.php
@@ -3,7 +3,10 @@
 $conexion = "interoperacion";
 $esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB ( $conexion );
 
+if(isset($_REQUEST['valor'])){
 	$cadenaSql = $this->sql->getCadenaSql ( 'obtenerConsumo', $_REQUEST['valor'] );
 	$resultadoItems = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" );
 	echo json_encode ( $resultadoItems );
+}
+
 ?>
