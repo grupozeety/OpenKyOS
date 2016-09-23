afe6cc801f1dfa3399065adcc4d67e60447cd730
stiv15 <stivmarquez.ud@gmail.com>
2016-09-23 15:38:59 -0500

    Desarrollo Parcial Funcionalidad para crear Hoja de calculo como Reporte

--- blocks/reportes/instalacionesGenerales/entidad/crearDocumentoHojaCalculo.php
+++ blocks/reportes/instalacionesGenerales/entidad/crearDocumentoHojaCalculo.php
@@ -1,6 +1,20 @@
 <?php
 namespace reportes\instalacionesGenerales\entidad;
 
+$ruta = $this->miConfigurador->getVariableConfiguracion("raizDocumento");
+$host = $this->miConfigurador->getVariableConfiguracion("host") . $this->miConfigurador->getVariableConfiguracion("site") . "/plugin/html2pfd/";
+
+//include $ruta . "/plugin/html2pdf/html2pdf.class.php";
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+date_default_timezone_set('Europe/London');
+
+define('EOL', (PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
+require_once $ruta . "/plugin/PHPExcel/Classes/PHPExcel.php";
+
 class GenerarReporteExcelInstalaciones {
 
     public $miConfigurador;
@@ -9,14 +23,61 @@ class GenerarReporteExcelInstalaciones {
     public $miSql;
     public $conexion;
     public $proyectos;
+    public $objCal;
 
     public function __construct($sql, $proyectos) {
 
         $this->miConfigurador = \Configurador::singleton();
         $this->miConfigurador->fabricaConexiones->setRecursoDB('principal');
         $this->miSql = $sql;
-
         $this->proyectos = $proyectos;
+
+        $objPHPExcel = new \PHPExcel();
+
+        // Set document properties
+        $objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
+                    ->setLastModifiedBy("Maarten Balliauw")
+                    ->setTitle("Office 2007 XLSX Test Document")
+                    ->setSubject("Office 2007 XLSX Test Document")
+                    ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+                    ->setCategory("Test result file");
+
+        // Add some data
+        $objPHPExcel->setActiveSheetIndex(0)
+                    ->setCellValue('A1', 'Hello')
+                    ->setCellValue('B2', 'world!')
+                    ->setCellValue('C1', 'Hello')
+                    ->setCellValue('D2', 'world!');
+
+        // Miscellaneous glyphs, UTF-8
+        $objPHPExcel->setActiveSheetIndex(0)
+                    ->setCellValue('A4', 'Miscellaneous glyphs')
+                    ->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
+
+        // Rename worksheet
+        $objPHPExcel->getActiveSheet()->setTitle('Simple');
+
+        // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+        $objPHPExcel->setActiveSheetIndex(0);
+
+        // Redirect output to a client’s web browser (Excel2007)
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="01simple.xlsx"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+
+        // If you're serving to IE over SSL, then the following may be needed
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+        header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header('Pragma: public'); // HTTP/1.0
+        ob_clean();
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $objWriter->save('php://output');
+
+        exit;
+
         var_dump($this->proyectos);
         var_dump($this->proyectos[0]['paquetesTrabajo']);
 
