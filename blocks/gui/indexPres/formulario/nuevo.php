00f66e0ac2fc2366ab81905ccdd44c615c715353
violetasdev <vsosa.ud@gmail.com>
2016-10-05 00:42:10 -0500

    Actualización interfaz

--- blocks/gui/indexPres/formulario/nuevo.php
+++ blocks/gui/indexPres/formulario/nuevo.php
@@ -12,12 +12,7 @@ $directorio .= $this->miConfigurador->getVariableConfiguracion("enlace");
 
 
 <div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 1800px; height: 600px; overflow: hidden;">
-    <!-- Slides Container -->
     <div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 1800px; height: 600px; overflow: hidden;">
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/abstract_2.jpg" /></div>
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/abstract_3.jpg" /></div>
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/abstract_4.jpg" /></div>
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/abstract_5.jpg" /></div>
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/abstract_6.jpg" /></div>
+        <div><img u="image" src="<?php echo $rutaBloque ?>/images/wallpaper_vector_violeta.png" /></div>
     </div>
 </div>
