cd3882c2b5f8cae424de7537c97afd31f0c26c54
stiv15 <stivmarquez.ud@gmail.com>
2016-10-12 13:59:26 -0500

    Bloque Inicial de Desarrollo

---
+++ blocks/gestionComisionamiento/generacionActas/frontera/script/ajax.php
@@ -0,0 +1,13 @@
+<?php
+/** 
+ * Código Correspondiente a las Url de la peticiones Ajax.
+ */
+?>
+<script type='text/javascript'>
+
+/** 
+ * Código JavaScript Correspondiente a la utilización de las Peticiones Ajax.
+ */
+
+</script>
+
