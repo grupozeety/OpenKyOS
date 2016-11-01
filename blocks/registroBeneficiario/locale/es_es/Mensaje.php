aaa8924293ed84079b76cd887ce3be70f6105060
stiv15 <stivmarquez.ud@gmail.com>
2016-11-01 11:29:44 -0500

    Ajuste Registra Beneficiario

--- blocks/registroBeneficiario/locale/es_es/Mensaje.php
+++ blocks/registroBeneficiario/locale/es_es/Mensaje.php
@@ -10,8 +10,8 @@ $this->idioma["noDefinido"] = "";
 $this->idioma["botonIngresar"] = "Ingresar";
 $this->idioma["botonAgregar"] = "Agregar";
 $this->idioma["botonContinuar"] = "Continuar";
-$this->idioma["confirma"]="Se ha registrado correctamente el Beneficiario/Cliente Potencial. Carpeta sincronizada en el Gestor Documental";
-$this->idioma["confirmaAlfresco"]="Se ha registrado correctamente el Beneficiario/Cliente Potencial. ERROR Carpeta NO sincronizada en el Gestor Documental";
+$this->idioma["confirma"] = "Se ha registrado correctamente el Beneficiario/Cliente Potencial. Carpeta sincronizada en el Gestor Documental";
+$this->idioma["confirmaAlfresco"] = "Se ha registrado correctamente el Beneficiario/Cliente Potencial. ERROR Carpeta NO sincronizada en el Gestor Documental";
 $this->idioma["error"] = "No se ha registrado correctamente el Beneficiario/Cliente Potencial.";
 $this->idioma["regresarConsultar"] = "Aceptar";
 
@@ -61,7 +61,7 @@ $this->idioma["geolocalizacion"] = "Geolocalización";
 $this->idioma["mensajeRegistro"] = "Se ha registrado correctamente el Beneficiario/Cliente Potencial.";
 $this->idioma["facebook"] = "Facebook";
 
-$this->idioma["mensajeRegistro"]=$this->idioma["confirma"]="Se ha registrado correctamente el Beneficiario/Cliente Potencial. Carpeta sincronizada en el Gestor Documental";
-$this->idioma["mensajeAlfresco"]="Se ha registrado correctamente el Beneficiario/Cliente Potencial. ERROR Carpeta NO sincronizada en el Gestor Documental";
+$this->idioma["mensajeRegistro"] = "Se ha registrado correctamente el Beneficiario/Cliente Potencial. Carpeta sincronizada en el Gestor Documental";
+$this->idioma["mensajeAlfresco"] = "Se ha registrado correctamente el Beneficiario/Cliente Potencial. ERROR Carpeta NO sincronizada en el Gestor Documental";
 
 ?>
