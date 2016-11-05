02ef0363c17ba07bfeacb97801f80ce578b35f51
stiv15 <stivmarquez.ud@gmail.com>
2016-11-05 09:42:23 -0500

    Ajuste LLamar Api

--- blocks/llamarApi/Sql.class.php
+++ blocks/llamarApi/Sql.class.php
@@ -2,106 +2,47 @@
 
 namespace llamarApi\funcion;
 
-if (! isset ( $GLOBALS ["autorizado"] )) {
-	include ("../index.php");
-	exit ();
+if (!isset($GLOBALS["autorizado"])) {
+    include "../index.php";
+    exit();
 }
 
-include_once ("core/manager/Configurador.class.php");
-include_once ("core/connection/Sql.class.php");
+include_once "core/manager/Configurador.class.php";
+include_once "core/connection/Sql.class.php";
 
 // Para evitar redefiniciones de clases el nombre de la clase del archivo sqle debe corresponder al nombre del bloque
 // en camel case precedida por la palabra sql
 class Sql extends \Sql {
-	var $miConfigurador;
-	function __construct() {
-		$this->miConfigurador = \Configurador::singleton ();
-	}
-	function getCadenaSql($tipo, $variable = "") {
-		
-		/**
-		 * 1.
-		 * Revisar las variables para evitar SQL Injection
-		 */
-		$prefijo = $this->miConfigurador->getVariableConfiguracion ( "prefijo" );
-		$idSesion = $this->miConfigurador->getVariableConfiguracion ( "id_sesion" );
-		
-		switch ($tipo) {
-			
-			/**
-			 * Clausulas genéricas.
-			 * se espera que estén en todos los formularios
-			 * que utilicen esta plantilla
-			 */
-			case "iniciarTransaccion" :
-				$cadenaSql = "START TRANSACTION";
-				break;
-			
-			case "finalizarTransaccion" :
-				$cadenaSql = "COMMIT";
-				break;
-			
-			case "cancelarTransaccion" :
-				$cadenaSql = "ROLLBACK";
-				break;
-			
-			case "eliminarTemp" :
-				
-				$cadenaSql = "DELETE ";
-				$cadenaSql .= "FROM ";
-				$cadenaSql .= $prefijo . "tempFormulario ";
-				$cadenaSql .= "WHERE ";
-				$cadenaSql .= "id_sesion = '" . $variable . "' ";
-				break;
-			
-			case "insertarTemp" :
-				$cadenaSql = "INSERT INTO ";
-				$cadenaSql .= $prefijo . "tempFormulario ";
-				$cadenaSql .= "( ";
-				$cadenaSql .= "id_sesion, ";
-				$cadenaSql .= "formulario, ";
-				$cadenaSql .= "campo, ";
-				$cadenaSql .= "valor, ";
-				$cadenaSql .= "fecha ";
-				$cadenaSql .= ") ";
-				$cadenaSql .= "VALUES ";
-				
-				foreach ( $_REQUEST as $clave => $valor ) {
-					$cadenaSql .= "( ";
-					$cadenaSql .= "'" . $idSesion . "', ";
-					$cadenaSql .= "'" . $variable ['formulario'] . "', ";
-					$cadenaSql .= "'" . $clave . "', ";
-					$cadenaSql .= "'" . $valor . "', ";
-					$cadenaSql .= "'" . $variable ['fecha'] . "' ";
-					$cadenaSql .= "),";
-				}
-				
-				$cadenaSql = substr ( $cadenaSql, 0, (strlen ( $cadenaSql ) - 1) );
-				break;
-			
-			case "rescatarTemp" :
-				$cadenaSql = "SELECT ";
-				$cadenaSql .= "id_sesion, ";
-				$cadenaSql .= "formulario, ";
-				$cadenaSql .= "campo, ";
-				$cadenaSql .= "valor, ";
-				$cadenaSql .= "fecha ";
-				$cadenaSql .= "FROM ";
-				$cadenaSql .= $prefijo . "tempFormulario ";
-				$cadenaSql .= "WHERE ";
-				$cadenaSql .= "id_sesion='" . $idSesion . "'";
-				break;
-			
-			/* Consultas del desarrollo */
-			
-				
-				case "asignar" :
-					$cadenaSql = "INSERT ";
-					break;
-		}
-		
-		return $cadenaSql;
-	}
+    public $miConfigurador;
+    public function __construct() {
+        $this->miConfigurador = \Configurador::singleton();
+    }
+    public function getCadenaSql($tipo, $variable = "") {
+
+        /**
+         * 1.
+         * Revisar las variables para evitar SQL Injection
+         */
+        $prefijo = $this->miConfigurador->getVariableConfiguracion("prefijo");
+        $idSesion = $this->miConfigurador->getVariableConfiguracion("id_sesion");
+
+        switch ($tipo) {
+
+            /**
+             * Clausulas genéricas.
+             * se espera que estén en todos los formularios
+             * que utilicen esta plantilla
+             */
+            case 'consultarInformacionApi':
+                $cadenaSql = " SELECT componente, host, usuario, password, token_codificado, ruta_cookie ";
+                $cadenaSql .= " FROM parametros.api_data";
+                $cadenaSql .= " WHERE componente ='" . $variable . "';";
+                break;
+
+        }
+
+        return $cadenaSql;
+    }
 }
 
 ?>
