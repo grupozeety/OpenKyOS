c563ad85d1dbe4b16989072e667109a469280ace
emmanueltaborda2015 <tabordaemmanuel@gmail.com>
2016-11-06 10:47:05 -0500

    Implementando Transacciones y adicionando campo MINVI.

--- blocks/registroBeneficiario/locale/es_es/Mensaje.php
+++ blocks/registroBeneficiario/locale/es_es/Mensaje.php
@@ -12,7 +12,7 @@ $this->idioma["botonAgregar"] = "Agregar";
 $this->idioma["botonContinuar"] = "Continuar";
 $this->idioma["confirma"] = "Se ha registrado correctamente el Beneficiario/Cliente Potencial. Carpeta sincronizada en el Gestor Documental";
 
-$this->idioma["minvi"] = "MINVI";
+$this->idioma["minvi"] = "Registrado en Ministerio de Vivienda";
 
 $this->idioma["noAlfresco"] = "Error en el proceso de sincronización con Alfresco. Comunicar al Administrador.";
 $this->idioma["error"] = "No se ha registrado correctamente el Beneficiario/Cliente Potencial.";
