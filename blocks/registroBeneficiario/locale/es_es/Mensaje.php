4c0cb45be5b0976263d3a81568f809d744118941
violetasdev <vsosa.ud@gmail.com>
2016-11-06 07:03:47 -0500

    mensajes redireccion alfresco

--- blocks/registroBeneficiario/locale/es_es/Mensaje.php
+++ blocks/registroBeneficiario/locale/es_es/Mensaje.php
@@ -11,7 +11,8 @@ $this->idioma["botonIngresar"] = "Ingresar";
 $this->idioma["botonAgregar"] = "Agregar";
 $this->idioma["botonContinuar"] = "Continuar";
 $this->idioma["confirma"] = "Se ha registrado correctamente el Beneficiario/Cliente Potencial. Carpeta sincronizada en el Gestor Documental";
-$this->idioma["confirmaAlfresco"] = "Error en el proceso de sincronización con Alfresco. Comunicar al Administrador.";
+
+$this->idioma["noAlfresco"] = "Error en el proceso de sincronización con Alfresco. Comunicar al Administrador.";
 $this->idioma["error"] = "No se ha registrado correctamente el Beneficiario/Cliente Potencial.";
 $this->idioma["regresarConsultar"] = "Aceptar";
 
