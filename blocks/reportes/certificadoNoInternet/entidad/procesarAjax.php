103208eb0c7f300adeadcbb25f85c80acf7545c3
violetasdev <vsosa.ud@gmail.com>
2016-11-10 10:50:11 -0500

    optimización consulta certificado internet

--- blocks/reportes/certificadoNoInternet/entidad/procesarAjax.php
+++ blocks/reportes/certificadoNoInternet/entidad/procesarAjax.php
@@ -1,5 +1,8 @@
 <?php
 namespace reportes\certificadoNoInternet\entidad;
+
+include_once "core/auth/SesionSso.class.php";
+
 class procesarAjax {
     public $miConfigurador;
     public $sql;
@@ -13,10 +16,25 @@ class procesarAjax {
         $conexion = "interoperacion";
         $esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
 
+        $sesion = \SesionSso::singleton ();
+        $respuesta = $sesion->getParametrosSesionAbierta ();
+        $rol = $respuesta ['description'] [0];
+        $idusuario = $respuesta ['mail'] [0];
+        if ($rol == 'Comisionador') {
+        	$comisionador = true;
+        } else {
+        	$comisionador = false;
+        }
+         
         switch ($_REQUEST['funcion']) {
 
             case 'consultaBeneficiarios':
-                $cadenaSql = $this->sql->getCadenaSql('consultarBeneficiariosPotenciales');
+            	$cadena='';
+            	if($comisionador==true){
+            		$cadena="AND id_comisionador='".$idusuario."'";
+            	}
+            	
+               $cadenaSql = $this->sql->getCadenaSql('consultarBeneficiariosPotenciales', $cadena);
 
                 $resultadoItems = $esteRecursoDB->ejecutarAcceso($cadenaSql, "busqueda");
 
