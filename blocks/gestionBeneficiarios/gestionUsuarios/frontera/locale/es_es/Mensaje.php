66b28294b5e4867990c7f5669beb34391d084c04
emmanueltaborda2015 <tabordaemmanuel@gmail.com>
2016-12-21 20:46:02 -0500

    Gestion Usuarios LDAP-OpenKyOS

---
+++ blocks/gestionBeneficiarios/gestionUsuarios/frontera/locale/es_es/Mensaje.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Etiquetas correspondientes a los Formularios
+ */
+
+$this->idioma['noDefinido'] = '';
+
+$this->idioma["contrasena"] = "Contraseña";
+
+$this->idioma['contrasena_val'] = 'Confirmación de Contraseña';
+
+$this->idioma['correo_electronico'] = 'Correo electrónico';
+
+$this->idioma['nombre_completo'] = 'Nombre Completo';
+
+$this->idioma['nombre_usuario'] = 'Nombre de Usuario';
+
+$this->idioma['telefono'] = 'Número de Teléfono o Celular';
+
+$this->idioma['rol'] = 'Rol';
+
+$this->idioma['botonContinuar'] = 'Crear';
+
+
+?>
