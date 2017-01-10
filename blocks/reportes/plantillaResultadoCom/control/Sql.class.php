ac15980f84a0c77502f2e8497df27c65ce765351
violetasdev <vsosa.ud@gmail.com>
2017-01-09 19:05:58 -0500

    Cargue Masivo Pruebas de Comisionamiento

---
+++ blocks/reportes/plantillaResultadoCom/control/Sql.class.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace reportes\plantillaResultadoCom;
+
+if (! isset ( $GLOBALS ["autorizado"] )) {
+	include "../index.php";
+	exit ();
+}
+
+include_once "core/manager/Configurador.class.php";
+include_once "core/connection/Sql.class.php";
+
+// Para evitar redefiniciones de clases el nombre de la clase del archivo sqle debe corresponder al nombre del bloque
+// en camel case precedida por la palabra sql
+class Sql extends \Sql {
+	public $miConfigurador;
+	public function getCadenaSql($tipo, $variable = '') {
+		
+		/**
+		 * 1.
+		 * Revisar las variables para evitar SQL Injection
+		 */
+		$prefijo = $this->miConfigurador->getVariableConfiguracion ( "prefijo" );
+		$idSesion = $this->miConfigurador->getVariableConfiguracion ( "id_sesion" );
+		
+		switch ($tipo) {
+			
+			/**
+			 * Clausulas específicas
+			 */
+			
+			// Validaciones
+			
+			case 'consultarExitenciaActa' :
+				$cadenaSql = " SELECT ep.id as identificador_acta, cn.numero_identificacion ";
+				$cadenaSql .= " FROM interoperacion.acta_entrega_portatil ep";
+				$cadenaSql .= " JOIN interoperacion.contrato cn ON cn.id_beneficiario=ep.id_beneficiario AND cn.estado_registro='TRUE'";
+				$cadenaSql .= " WHERE ep.estado_registro='TRUE'";
+				$cadenaSql .= " AND ep.serial='" . $variable ['serial_portatil'] . "'";
+				// $cadenaSql .= " AND cn.numero_identificacion='" . $variable['identificacion_beneficiario'] . "'";
+				break;
+			
+			case 'consultarExitenciaSerialPortatil' :
+				$cadenaSql = " SELECT ep.id as identificador_acta, cn.numero_identificacion ";
+				$cadenaSql .= " FROM interoperacion.acta_entrega_portatil ep";
+				$cadenaSql .= " JOIN interoperacion.contrato cn ON cn.id_beneficiario=ep.id_beneficiario AND cn.estado_registro='TRUE'";
+				$cadenaSql .= " WHERE ep.estado_registro='TRUE'";
+				$cadenaSql .= " AND ep.serial='" . $variable ['serial_portatil'] . "'";
+				// $cadenaSql .= " AND cn.numero_identificacion='" . $variable['identificacion_beneficiario'] . "'";
+				break;
+			
+			case 'consultarExitenciaContrato' :
+				$cadenaSql = " SELECT id_beneficiario, numero_contrato,numero_identificacion";
+				$cadenaSql .= " FROM interoperacion.contrato";
+				$cadenaSql .= " WHERE estado_registro='TRUE'";
+				$cadenaSql .= " AND numero_identificacion='" . $variable . "';";
+				break;
+			
+			case 'consultarExitenciaBeneficiario' :
+				$cadenaSql = " SELECT id_beneficiario";
+				$cadenaSql .= " FROM interoperacion.beneficiario_potencial";
+				$cadenaSql .= " WHERE estado_registro='TRUE'";
+				$cadenaSql .= " AND identificacion='" . $variable . "';";
+				break;
+			
+			case 'consultarExistenciaServicio' :
+				$cadenaSql = " SELECT id_beneficiario";
+				$cadenaSql .= " FROM interoperacion.acta_entrega_servicios";
+				$cadenaSql .= " WHERE estado_registro='TRUE'";
+				$cadenaSql .= " AND identificacion='" . $variable . "';";
+				break;
+			
+			case 'consultarInformacionBeneficiario' :
+				$cadenaSql = " SELECT bp.* ,mn.municipio as nombre_municipio,dp.departamento as nombre_departamento";
+				$cadenaSql .= " FROM interoperacion.beneficiario_potencial bp";
+				$cadenaSql .= " JOIN parametros.municipio mn ON mn.codigo_mun=bp.municipio";
+				$cadenaSql .= " JOIN parametros.departamento dp ON dp.codigo_dep=bp.departamento";
+				$cadenaSql .= " WHERE bp.estado_registro='TRUE' ";
+				$cadenaSql .= " AND bp.identificacion='" . $variable . "';";
+				
+				break;
+			
+			// Registro Procesos
+			case 'registrarProceso' :
+				$cadenaSql = " INSERT INTO parametros.procesos_masivos(";
+				$cadenaSql .= " descripcion,";
+				$cadenaSql .= " estado,";
+				$cadenaSql .= " nombre_archivo, ruta_archivo,";
+				$cadenaSql .= " parametro_inicio,";
+				$cadenaSql .= " parametro_fin)";
+				$cadenaSql .= " VALUES (";
+				$cadenaSql .= " 'Actualizar Servicio - Pruebas Comision',";
+				$cadenaSql .= " 'Actualizar',";
+				$cadenaSql .= " '" . $variable['nombre_archivo']. "',";
+				$cadenaSql .= " '" . $variable['nombre_archivo']. "',";
+				$cadenaSql .= " 0,";
+				$cadenaSql .= " 0";
+				$cadenaSql .= " )RETURNING id_proceso;";
+				break;
+			
+			case 'consultarProceso' :
+				$cadenaSql = " SELECT * ";
+				$cadenaSql .= " FROM parametros.procesos_masivos";
+				$cadenaSql .= " WHERE descripcion='Contratos'";
+				$cadenaSql .= " ORDER BY id_proceso DESC;";
+				break;
+			
+			case 'consultarProcesoParticular' :
+				$cadenaSql = " SELECT *";
+				$cadenaSql .= " FROM parametros.procesos_masivos";
+				$cadenaSql .= " WHERE id_proceso=(";
+				$cadenaSql .= " SELECT MIN(id_proceso) ";
+				$cadenaSql .= " FROM parametros.procesos_masivos";
+				$cadenaSql .= " WHERE estado_registro='TRUE' ";
+				$cadenaSql .= " AND estado='No Iniciado'";
+				$cadenaSql .= " );";
+				break;
+			
+			case 'actualizarProceso' :
+				$cadenaSql = " UPDATE parametros.procesos_masivos";
+				$cadenaSql .= " SET estado='En Proceso'";
+				$cadenaSql .= " WHERE id_proceso='" . $variable . "';";
+				break;
+			
+			case 'finalizarProceso' :
+				$cadenaSql = " UPDATE parametros.procesos_masivos";
+				$cadenaSql .= " SET estado='Finalizado',";
+				$cadenaSql .= " ruta_archivo='" . $variable ['ruta_archivo'] . "',";
+				$cadenaSql .= " nombre_ruta_archivo='" . $variable ['nombre_archivo'] . "'";
+				$cadenaSql .= " WHERE id_proceso='" . $variable ['id_proceso'] . "';";
+				break;
+			// Crear Documenntos Contrato
+			case 'ConsultaBeneficiarios' :
+				$cadenaSql = " SELECT *";
+				$cadenaSql .= " FROM interoperacion.contrato";
+				$cadenaSql .= " WHERE numero_contrato >=" . $variable ['Inicio'] . " ";
+				$cadenaSql .= " AND numero_contrato<=" . $variable ['Fin'] . " ";
+				$cadenaSql .= " ORDER BY numero_contrato ;";
+				break;
+			
+			case 'consultarTipoDocumento' :
+				$cadenaSql = " SELECT pr.id_parametro,pr.codigo, pr.descripcion ";
+				$cadenaSql .= " FROM parametros.parametros pr";
+				$cadenaSql .= " JOIN parametros.relacion_parametro rl ON rl.id_rel_parametro=pr.rel_parametro";
+				$cadenaSql .= " WHERE ";
+				$cadenaSql .= " pr.estado_registro=TRUE ";
+				$cadenaSql .= " AND rl.descripcion='Tipo de Documento'";
+				$cadenaSql .= " AND pr.descripcion='" . $variable . "' ";
+				$cadenaSql .= " AND rl.estado_registro=TRUE ";
+				break;
+			
+			case 'consultarParametroParticular' :
+				$cadenaSql = " SELECT descripcion ";
+				$cadenaSql .= " FROM parametros.parametros";
+				$cadenaSql .= " WHERE estado_registro='TRUE'";
+				$cadenaSql .= " AND id_parametro='" . $variable . "';";
+				break;
+			
+			case 'actualizarServicio' :
+				$cadenaSql = " UPDATE interoperacion.acta_entrega_servicios ";
+				$cadenaSql .= " SET ";
+				$cadenaSql .= " resultado_vs='".$variable['resultado_vs']."', ";
+				$cadenaSql .= " resultado_vb='".$variable['resultado_vb']."', ";
+				$cadenaSql .= " resultado_p1='".$variable['resultado_p1']."', ";
+				$cadenaSql .= " observaciones_p1='".$variable['observaciones_p1']."', ";
+				$cadenaSql .= " resultado_tr2='".$variable['resultado_tr2']."', ";
+				$cadenaSql .= " resultado_tr1='".$variable['resultado_tr1']."', ";
+				$cadenaSql .= " reporte_fallos='Fecha Comisionamiento ".$variable['fecha'].". ".$variable['reporte_fallos']."', ";
+				$cadenaSql .= " acceso_reportando='".$variable['acceso_reportando']."', ";
+				$cadenaSql .= " hora_prueba_vs='".$variable['fecha']."', ";
+				$cadenaSql .= " hora_prueba_vb='".$variable['fecha']."', ";
+				$cadenaSql .= " hora_prueba_p1='".$variable['fecha']."', ";
+				$cadenaSql .= " hora_prueba_tr2='".$variable['fecha']."', ";
+				$cadenaSql .= " hora_prueba_tr1='".$variable['fecha']."', ";
+				$cadenaSql .= " paginas_visitadas='".$variable['paginas_visitadas']."' ";
+		     	$cadenaSql .= " WHERE id_beneficiario='" . $variable['id_beneficiario'] . "' ;";
+				break;
+		}
+		
+		return $cadenaSql;
+	}
+}
+?>
+
---
+++ blocks/reportes/plantillaResultadoCom/entidad/cargarInformacion.php
@@ -0,0 +1,303 @@
+<?php
+
+namespace reportes\plantillaResultadoCom\entidad;
+
+if (! isset ( $GLOBALS ["autorizado"] )) {
+	include "../index.php";
+	exit ();
+}
+
+$ruta = $this->miConfigurador->getVariableConfiguracion ( "raizDocumento" );
+$host = $this->miConfigurador->getVariableConfiguracion ( "host" ) . $this->miConfigurador->getVariableConfiguracion ( "site" ) . "/plugin/html2pfd/";
+
+require_once $ruta . "/plugin/PHPExcel/Classes/PHPExcel.php";
+
+// require_once $ruta . "/plugin/PHPExcel/Classes/PHPExcel/Reader/Excel2007.php";
+
+require_once $ruta . "/plugin/PHPExcel/Classes/PHPExcel/IOFactory.php";
+
+include_once 'Redireccionador.php';
+class FormProcessor {
+	public $miConfigurador;
+	public $lenguaje;
+	public $miFormulario;
+	public $miSql;
+	public $conexion;
+	public $esteRecursoDB;
+	public $rutaURL;
+	public $rutaAbsoluta;
+	public function __construct($lenguaje, $sql) {
+		date_default_timezone_set ( 'America/Bogota' );
+		
+		$this->miConfigurador = \Configurador::singleton ();
+		$this->miConfigurador->fabricaConexiones->setRecursoDB ( 'principal' );
+		$this->lenguaje = $lenguaje;
+		$this->miSql = $sql;
+		
+		$this->rutaURL = $this->miConfigurador->getVariableConfiguracion ( "host" ) . $this->miConfigurador->getVariableConfiguracion ( "site" );
+		
+		$this->rutaAbsoluta = $this->miConfigurador->getVariableConfiguracion ( "raizDocumento" );
+		
+		if (! isset ( $_REQUEST ["bloqueGrupo"] ) || $_REQUEST ["bloqueGrupo"] == "") {
+			$this->rutaURL .= "/blocks/" . $_REQUEST ["bloque"] . "/";
+			$this->rutaAbsoluta .= "/blocks/" . $_REQUEST ["bloque"] . "/";
+		} else {
+			$this->rutaURL .= "/blocks/" . $_REQUEST ["bloqueGrupo"] . "/" . $_REQUEST ["bloque"] . "/";
+			$this->rutaAbsoluta .= "/blocks/" . $_REQUEST ["bloqueGrupo"] . "/" . $_REQUEST ["bloque"] . "/";
+		}
+		// Conexion a Base de Datos
+		$conexion = "interoperacion";
+		$this->esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB ( $conexion );
+		
+		$_REQUEST ['tiempo'] = time ();
+		
+		/**
+		 * 1.
+		 * Cargar Archivo en el Directorio
+		 */
+		
+		$this->cargarArchivos ();
+		
+		/**
+		 * 2.
+		 * Cargar Informacion Hoja de Calculo
+		 */
+		
+		$this->cargarInformacionHojaCalculo ();
+		
+		/**
+		 * 4.
+		 * Validar Existencia Contratos Beneficiarios
+		 */
+		
+		$this->validarContratosExistentes ();
+		
+		/**
+		 * 5.
+		 * Validar Existencia Beneficiarios
+		 */
+		
+		$this->validarBeneficiariosExistentes ();
+		
+		/**
+		 * 6.
+		 * Validar Existencia Acta de Servicio
+		 */
+		
+		$this->validarServiciosExistentes ();
+		
+		/**
+		 * 6.
+		 * Procesar Información Beneficiarios
+		 */
+		
+		$this->procesarInformacionBeneficiario ();
+		
+		/**
+		 * 7.
+		 * Crear Contrato
+		 */
+		
+		$this->informacionServicio ();
+		
+		/**
+		 * 9.
+		 * Registrar Tarea o Proceso Masivo
+		 */
+		
+		$this->registroProceso ();
+		
+		if (isset ( $this->proceso ) && $this->proceso != null) {
+			Redireccionador::redireccionar ( "ExitoRegistroProceso", $this->proceso );
+		} else {
+			Redireccionador::redireccionar ( "ErrorRegistroProceso" );
+		}
+	}
+	
+	/**
+	 * Funcionalidades Específicas
+	 */
+	public function registroProceso() {
+		$arreglo_registro = array (
+				'nombre_archivo' => $this->archivo ['ruta_archivo'] 
+		);
+		
+		$cadenaSql = $this->miSql->getCadenaSql ( 'registrarProceso', $arreglo_registro );
+		$this->proceso = $this->esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" ) [0] ['id_proceso'];
+	}
+	public function informacionServicio() {
+		foreach ( $this->informacion_registrar as $key => $value ) {
+			$cadenaSql = $this->miSql->getCadenaSql ( 'actualizarServicio', $value );
+			$resultado = $this->esteRecursoDB->ejecutarAcceso ( $cadenaSql, "registro" );
+		
+			if ($resultado != true) {
+				Redireccionador::redireccionar ( "ErrorActualizacion" );
+			}
+		}
+	}
+	public function procesarInformacionBeneficiario() {
+		foreach ( $this->datos_beneficiario as $key => $value ) {
+			
+			$cadenaSql = $this->miSql->getCadenaSql ( 'consultarInformacionBeneficiario', $value ['identificacion_beneficiario'] );
+			
+			$consulta = $this->esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" ) [0];
+			
+			$this->informacion_registrar [] = array (
+					'id_beneficiario' => $consulta ['id_beneficiario'],
+					'resultado_vs' => $value ['subida'],
+					'resultado_vb' => $value ['bajada'],
+					'resultado_p1' => $value ['latencia'],
+					'observaciones_p1' => $value ['pagina'],
+					'resultado_tr2' => $value ['tracert'],
+					'resultado_tr1' => $value ['estado'],
+					'reporte_fallos' => $value ['observaciones'],
+					'acceso_reportando' => $value ['tracert'],
+					'paginas_visitadas' => $value ['pagina'],
+					'fecha' => $value ['fecha_comisionamiento'] 
+			);
+		}
+	}
+	public function validarBeneficiariosExistentes() {
+		foreach ( $this->datos_beneficiario as $key => $value ) {
+			
+			$cadenaSql = $this->miSql->getCadenaSql ( 'consultarExitenciaBeneficiario', $value ['identificacion_beneficiario'] );
+			
+			$consulta = $this->esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" ) [0];
+			
+			if (is_null ( $consulta )) {
+				
+				Redireccionador::redireccionar ( "ErrorCreacionContratos" );
+			}
+		}
+	}
+	public function validarContratosExistentes() {
+		foreach ( $this->datos_beneficiario as $key => $value ) {
+			
+			$cadenaSql = $this->miSql->getCadenaSql ( 'consultarExitenciaContrato', $value ['identificacion_beneficiario'] );
+			$consulta = $this->esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" ) [0];
+			
+			if (is_null ( $consulta )) {
+				Redireccionador::redireccionar ( "ErrorCreacionContratos" );
+			}
+		}
+	}
+	public function validarServiciosExistentes() {
+		foreach ( $this->datos_beneficiario as $key => $value ) {
+			
+			$cadenaSql = $this->miSql->getCadenaSql ( 'consultarExistenciaServicio', $value ['identificacion_beneficiario'] );
+			
+			$consulta = $this->esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" ) [0];
+			
+			if (is_null ( $consulta )) {
+				$mensaje = " El beneficiario con identificación " . $value ['identificacion_beneficiario'] . " no tiene un equipo instalado. Sugerencia registrar el kit correspondiente.";
+				$this->escribir_log ( $mensaje );
+				
+				$this->error = true;
+			}
+		}
+	}
+	public function cargarInformacionHojaCalculo() {
+		ini_set ( 'memory_limit', '1024M' );
+		ini_set ( 'max_execution_time', 300 );
+		
+		if (file_exists ( $this->archivo ['ruta_archivo'] )) {
+			
+			$hojaCalculo = \PHPExcel_IOFactory::createReader ( $this->tipo_archivo );
+			$informacion = $hojaCalculo->load ( $this->archivo ['ruta_archivo'] );
+			
+			$informacion_general = $hojaCalculo->listWorksheetInfo ( $this->archivo ['ruta_archivo'] );
+			
+			{
+				$total_filas = $informacion_general [0] ['totalRows'];
+			}
+			
+			if ($total_filas > 501) {
+				Redireccionador::redireccionar ( "ErrorNoCargaInformacionHojaCalculo" );
+			}
+			
+			for($i = 2; $i <= $total_filas; $i ++) {
+				
+				$datos_beneficiario [$i] ['identificacion_beneficiario'] = $informacion->setActiveSheetIndex ()->getCell ( 'A' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['fecha_comisionamiento'] = $informacion->setActiveSheetIndex ()->getCell ( 'B' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['latencia'] = $informacion->setActiveSheetIndex ()->getCell ( 'C' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['tracert'] = $informacion->setActiveSheetIndex ()->getCell ( 'D' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['observaciones'] = $informacion->setActiveSheetIndex ()->getCell ( 'E' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['estado'] = $informacion->setActiveSheetIndex ()->getCell ( 'F' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['pagina'] = $informacion->setActiveSheetIndex ()->getCell ( 'G' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['subida'] = $informacion->setActiveSheetIndex ()->getCell ( 'H' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['bajada'] = $informacion->setActiveSheetIndex ()->getCell ( 'I' . $i )->getCalculatedValue ();
+			}
+			
+			$this->datos_beneficiario = $datos_beneficiario;
+			
+			unlink ( $this->archivo ['ruta_archivo'] );
+		} else {
+			Redireccionador::redireccionar ( "ErrorNoCargaInformacionHojaCalculo" );
+		}
+	}
+	public function cargarArchivos() {
+		$archivo_datos = '';
+		$archivo = $_FILES ['archivo_informacion'];
+		
+		if ($archivo ['error'] == 0) {
+			
+			switch ($archivo ['type']) {
+				case 'application/vnd.oasis.opendocument.spreadsheet' :
+					$this->tipo_archivo = 'OOCalc';
+					break;
+				
+				case 'application/vnd.ms-excel' :
+					$this->tipo_archivo = 'Excel5';
+					break;
+				
+				case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' :
+					$this->tipo_archivo = 'Excel2007';
+					break;
+				
+				default :
+					Redireccionador::redireccionar ( "ErrorFormatoArchivo" );
+					break;
+			}
+			
+			$this->prefijo = substr ( md5 ( uniqid ( time () ) ), 0, 6 );
+			/*
+			 * obtenemos los datos del Fichero
+			 */
+			$tamano = $archivo ['size'];
+			$tipo = $archivo ['type'];
+			$nombre_archivo = str_replace ( " ", "_", $archivo ['name'] );
+			/*
+			 * guardamos el fichero en el Directorio
+			 */
+			$ruta_absoluta = $this->rutaAbsoluta . "/entidad/archivos_validar/" . $this->prefijo . "_" . $nombre_archivo;
+			
+			$ruta_relativa = $this->rutaURL . " /entidad/archivos_validar/" . $this->prefijo . "_" . $nombre_archivo;
+			
+			$archivo ['rutaDirectorio'] = $ruta_absoluta;
+			
+			if (! copy ( $archivo ['tmp_name'], $ruta_absoluta )) {
+				
+				Redireccionador::redireccionar ( "ErrorCargarArchivo" );
+			}
+			
+			$this->archivo = array (
+					'ruta_archivo' => str_replace ( "//", "/", $ruta_absoluta ),
+					'nombre_archivo' => $archivo ['name'] 
+			);
+		} else {
+			Redireccionador::redireccionar ( "ErrorArchivoNoValido" );
+		}
+	}
+}
+
+$miProcesador = new FormProcessor ( $this->lenguaje, $this->sql );
+?>
+
