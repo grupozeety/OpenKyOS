a5deac4831ba3fbaa486cf5fe1b0357bfc0e1e18
violetasdev <vsosa.ud@gmail.com>
2017-01-12 22:38:27 -0500

    avance plantilla beneficiarios

---
+++ blocks/reportes/plantillaBeneficiario/entidad/Redireccionador.php
@@ -0,0 +1,82 @@
+<?php
+namespace reportes\plantillaBeneficiario\entidad;
+if (!isset($GLOBALS["autorizado"])) {
+    include "index.php";
+    exit();
+}
+class Redireccionador {
+    public static function redireccionar($opcion, $valor = "") {
+
+        $miConfigurador = \Configurador::singleton();
+
+        switch ($opcion) {
+
+            case "ErrorFormatoArchivo":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=errorFormatoArchivo';
+                break;
+
+            case "ErrorArchivoNoValido":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=errorArchivoNoValido';
+                break;
+
+            case "ErrorCargarArchivo":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=errorCargarArchivo';
+                break;
+
+            case "ErrorNoCargaInformacionHojaCalculo":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=errorCargarInformacion';
+                break;
+
+            case "ErrorInformacionCargar":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=errorInformacionCargar';
+                $variable .= '&log=' . $valor;
+                break;
+
+            case "ExitoInformacion":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=exitoInformacion';
+                break;
+
+            case "ErrorCreacionContratos":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=errorCreacionContratos';
+                break;
+
+            case "ExitoRegistroProceso":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=exitoRegistroProceso';
+                $variable .= '&proceso=' . $valor;
+                break;
+
+            case "ErrorRegistroProceso":
+                $variable = 'pagina=plantillaBeneficiario';
+                $variable .= '&mensajeModal=errorRegistroProceso';
+                break;
+                
+                case "ErrorActualizacion":
+                	$variable = 'pagina=plantillaBeneficiario';
+                	$variable .= '&mensajeModal=errorActualizacion';
+                	break;
+
+        }
+        foreach ($_REQUEST as $clave => $valor) {
+            unset($_REQUEST[$clave]);
+        }
+
+        $url = $miConfigurador->configuracion["host"] . $miConfigurador->configuracion["site"] . "/index.php?";
+        $enlace = $miConfigurador->configuracion['enlace'];
+        $variable = $miConfigurador->fabricaConexiones->crypto->codificar($variable);
+        $_REQUEST[$enlace] = $enlace . '=' . $variable;
+        $redireccion = $url . $_REQUEST[$enlace];
+
+        echo "<script>location.replace('" . $redireccion . "')</script>";
+
+        exit();
+    }
+}
+?>
---
+++ blocks/reportes/plantillaBeneficiario/entidad/validarInformacion.php
@@ -0,0 +1,287 @@
+<?php
+
+namespace reportes\plantillaBeneficiario\entidad;
+
+if (! isset ( $GLOBALS ["autorizado"] )) {
+	include "../index.php";
+	exit ();
+}
+
+$ruta = $this->miConfigurador->getVariableConfiguracion ( "raizDocumento" );
+$host = $this->miConfigurador->getVariableConfiguracion ( "host" ) . $this->miConfigurador->getVariableConfiguracion ( "site" ) . "/plugin/html2pfd/";
+
+require_once $ruta . "/plugin/PHPExcel/Classes/PHPExcel.php";
+
+// require_once $ruta . "/plugin/PHPExcel/Classes/PHPExcel/Reader/Excel2007.php";
+
+require_once $ruta . "/plugin/PHPExcel/Classes/PHPExcel/IOFactory.php";
+
+include_once 'Redireccionador.php';
+class FormProcessor {
+	public $miConfigurador;
+	public $lenguaje;
+	public $miFormulario;
+	public $miSql;
+	public $conexion;
+	public $archivos_datos;
+	public $esteRecursoDB;
+	public $datos_contrato;
+	public $rutaURL;
+	public $rutaAbsoluta;
+	public $clausulas;
+	public $registro_info_contrato;
+	public function __construct($lenguaje, $sql) {
+		date_default_timezone_set ( 'America/Bogota' );
+		$this->miConfigurador = \Configurador::singleton ();
+		$this->miConfigurador->fabricaConexiones->setRecursoDB ( 'principal' );
+		$this->lenguaje = $lenguaje;
+		$this->miSql = $sql;
+		
+		$this->rutaURL = $this->miConfigurador->getVariableConfiguracion ( "host" ) . $this->miConfigurador->getVariableConfiguracion ( "site" );
+		
+		$this->rutaAbsoluta = $this->miConfigurador->getVariableConfiguracion ( "raizDocumento" );
+		
+		if (! isset ( $_REQUEST ["bloqueGrupo"] ) || $_REQUEST ["bloqueGrupo"] == "") {
+			$this->rutaURL .= "/blocks/" . $_REQUEST ["bloque"] . "/";
+			$this->rutaAbsoluta .= "/blocks/" . $_REQUEST ["bloque"] . "/";
+		} else {
+			$this->rutaURL .= "/blocks/" . $_REQUEST ["bloqueGrupo"] . "/" . $_REQUEST ["bloque"] . "/";
+			$this->rutaAbsoluta .= "/blocks/" . $_REQUEST ["bloqueGrupo"] . "/" . $_REQUEST ["bloque"] . "/";
+		}
+		// Conexion a Base de Datos
+		$conexion = "interoperacion";
+		$this->esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB ( $conexion );
+		
+		$_REQUEST ['tiempo'] = time ();
+		
+		/**
+		 * 1.
+		 * Cargar Archivo en el Directorio
+		 */
+		
+		$this->cargarArchivos ();
+		
+		/**
+		 * 2.
+		 * Cargar Informacion Hoja de Calculo
+		 */
+		
+		$this->cargarInformacionHojaCalculo ();
+		
+		/**
+		 * 3.
+		 * Creación Log
+		 */
+		
+		$this->creacion_log ();
+		
+		/**
+		 * 4.
+		 * Validar Existencia Beneficiarios
+		 */
+		
+		if($_REQUEST['funcionalidad']==2){
+			$this->validarBeneficiariosExistentesRegistro ();
+		}else{
+			$this->validarBeneficiariosExistentes ();
+		}
+		
+		
+		/**
+		 * 7.
+		 * Cerrar Log
+		 */
+		
+		$this->cerrar_log ();
+		
+		if (isset ( $this->error )) {
+			Redireccionador::redireccionar ( "ErrorInformacionCargar", base64_encode ( $this->ruta_relativa_log ) );
+		} else {
+			Redireccionador::redireccionar ( "ExitoInformacion" );
+		}
+	}
+
+	public function validarBeneficiariosExistentes() {
+		foreach ( $this->datos_beneficiario as $key => $value ) {
+			
+			$cadenaSql = $this->miSql->getCadenaSql ( 'consultarExitenciaBeneficiario', $value ['identificacion_beneficiario'] );
+			
+			$consulta = $this->esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" ) [0];
+			
+			if (is_null ( $consulta )) {
+				$mensaje = " La identificación " . $value ['identificacion_beneficiario'] . ", no tiene asociado un beneficiario. Se sugiere registrarlo en el Sistema.";
+				$this->escribir_log ( $mensaje );
+				
+				$this->error = true;
+			}
+		}
+	}
+	
+	public function validarBeneficiariosExistentesRegistro() {
+		foreach ( $this->datos_beneficiario as $key => $value ) {
+				
+			$cadenaSql = $this->miSql->getCadenaSql ( 'consultarExitenciaBeneficiario', $value ['identificacion_beneficiario'] );
+				
+			$consulta = $this->esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" ) [0];
+				
+			if (!is_null( $consulta )) {
+				$mensaje = " La identificación " . $value ['identificacion_beneficiario'] . ", ya se encuentra registrada en el sistema.";
+				$this->escribir_log ( $mensaje );
+	
+				$this->error = true;
+			}
+		}
+	}
+
+	public function escribir_log($mensaje) {
+		fwrite ( $this->log, $mensaje . PHP_EOL );
+	}
+	public function cerrar_log() {
+		fclose ( $this->log );
+	}
+	public function creacion_log() {
+		$prefijo = substr ( md5 ( uniqid ( time () ) ), 0, 6 );
+		
+		$this->ruta_absoluta_log = $this->rutaAbsoluta . "/entidad/logs/Log_documento_validacion_" . $prefijo . ".log";
+		
+		$this->ruta_relativa_log = $this->rutaURL . "/entidad/logs/Log_documento_validacion_" . $prefijo . ".log";
+		
+		$this->log = fopen ( $this->ruta_absoluta_log, "w" );
+	}
+	public function cargarInformacionHojaCalculo() {
+		ini_set ( 'memory_limit', '1024M' );
+		ini_set ( 'max_execution_time', 300 );
+		
+		if (file_exists ( $this->archivo ['ruta_archivo'] )) {
+			
+			$hojaCalculo = \PHPExcel_IOFactory::createReader ( $this->tipo_archivo );
+			$informacion = $hojaCalculo->load ( $this->archivo ['ruta_archivo'] );
+			
+			$informacion_general = $hojaCalculo->listWorksheetInfo ( $this->archivo ['ruta_archivo'] );
+			
+			$total_filas = $informacion_general [0] ['totalRows'];
+			
+			if ($total_filas > 501) {
+				Redireccionador::redireccionar ( "ErrorNoCargaInformacionHojaCalculo" );
+			}
+			
+			for($i = 2; $i <= $total_filas; $i ++) {
+				
+				$datos_beneficiario [$i] ['departamento'] = $informacion->setActiveSheetIndex ()->getCell ( 'A' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['municipio'] = $informacion->setActiveSheetIndex ()->getCell ( 'B' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['id_proyecto'] = $informacion->setActiveSheetIndex ()->getCell ( 'C' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['proyecto'] = $informacion->setActiveSheetIndex ()->getCell ( 'D' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['tipo_beneficiario'] = $informacion->setActiveSheetIndex ()->getCell ( 'E' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['tipo_identificacion'] = $informacion->setActiveSheetIndex ()->getCell ( 'F' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['identificacion'] = $informacion->setActiveSheetIndex ()->getCell ( 'G' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['nombre'] = $informacion->setActiveSheetIndex ()->getCell ( 'H' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['primer_apellido'] = $informacion->setActiveSheetIndex ()->getCell ( 'I' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['segundo_apellido'] = $informacion->setActiveSheetIndex ()->getCell ( 'J' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['genero'] = $informacion->setActiveSheetIndex ()->getCell ( 'K' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['edad'] = $informacion->setActiveSheetIndex ()->getCell ( 'L' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['nivel_estudio'] = $informacion->setActiveSheetIndex ()->getCell ( 'M' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['correo'] = $informacion->setActiveSheetIndex ()->getCell ( 'N' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['telefono'] = $informacion->setActiveSheetIndex ()->getCell ( 'O' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['direccion'] = $informacion->setActiveSheetIndex ()->getCell ( 'P' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['manzana'] = $informacion->setActiveSheetIndex ()->getCell ( 'Q' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['bloque'] = $informacion->setActiveSheetIndex ()->getCell ( 'R' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['torre'] = $informacion->setActiveSheetIndex ()->getCell ( 'S' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['casa_apto'] = $informacion->setActiveSheetIndex ()->getCell ( 'T' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['interior'] = $informacion->setActiveSheetIndex ()->getCell ( 'U' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['lote'] = $informacion->setActiveSheetIndex ()->getCell ( 'V' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['piso'] = $informacion->setActiveSheetIndex ()->getCell ( 'W' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['minvivienda'] = $informacion->setActiveSheetIndex ()->getCell ( 'X' . $i )->getCalculatedValue ();
+				
+				$datos_beneficiario [$i] ['barrio'] = $informacion->setActiveSheetIndex ()->getCell ( 'Y' . $i )->getCalculatedValue ();
+
+			}
+			
+			unlink ( $this->archivo ['ruta_archivo'] );
+			
+			$this->datos_beneficiario = $datos_beneficiario;
+		} else {
+			Redireccionador::redireccionar ( "ErrorNoCargaInformacionHojaCalculo" );
+		}
+	}
+	public function cargarArchivos() {
+		$archivo_datos = '';
+		$archivo = $_FILES ['archivo_validacion'];
+		
+		if ($archivo ['error'] == 0) {
+			
+			switch ($archivo ['type']) {
+				case 'application/vnd.oasis.opendocument.spreadsheet' :
+					$this->tipo_archivo = 'OOCalc';
+					break;
+				
+				case 'application/vnd.ms-excel' :
+					$this->tipo_archivo = 'Excel5';
+					break;
+				
+				case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' :
+					$this->tipo_archivo = 'Excel2007';
+					break;
+				
+				default :
+					exit ();
+					Redireccionador::redireccionar ( "ErrorFormatoArchivo" );
+					break;
+			}
+			
+			$this->prefijo = substr ( md5 ( uniqid ( time () ) ), 0, 6 );
+			/*
+			 * obtenemos los datos del Fichero
+			 */
+			$tamano = $archivo ['size'];
+			$tipo = $archivo ['type'];
+			$nombre_archivo = str_replace ( " ", "_", $archivo ['name'] );
+			/*
+			 * guardamos el fichero en el Directorio
+			 */
+			$ruta_absoluta = $this->rutaAbsoluta . "entidad/archivos_validar/" . $this->prefijo . "_" . $nombre_archivo;
+			$ruta_relativa = $this->rutaURL . "entidad/archivos_validar/" . $this->prefijo . "_" . $nombre_archivo;
+			
+			$archivo ['rutaDirectorio'] = $ruta_absoluta;
+			
+			if (! copy ( $archivo ['tmp_name'], $ruta_absoluta )) {
+				
+				Redireccionador::redireccionar ( "ErrorCargarArchivo" );
+			}
+			
+			$this->archivo = array (
+					'ruta_archivo' => str_replace ( "//", "/", $ruta_absoluta ),
+					'nombre_archivo' => $archivo ['name'] 
+			);
+		} else {
+			
+			Redireccionador::redireccionar ( "ErrorArchivoNoValido" );
+		}
+	}
+}
+
+$miProcesador = new FormProcessor ( $this->lenguaje, $this->sql );
+?>
+
