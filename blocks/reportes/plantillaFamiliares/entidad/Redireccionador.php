ec489518c7379a6b0cbc6818b9e3d3085c23c8a2
stiv15 <stivmarquez.ud@gmail.com>
2017-01-17 17:10:42 -0500

    Desarrollo Parcial Masivo Familiares

---
+++ blocks/reportes/plantillaFamiliares/entidad/Redireccionador.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace reportes\plantillaFamiliares\entidad;
+
+if (!isset($GLOBALS["autorizado"])) {
+    include "index.php";
+    exit();
+}
+class Redireccionador {
+    public static function redireccionar($opcion, $valor = "") {
+        $miConfigurador = \Configurador::singleton();
+
+        switch ($opcion) {
+
+            case "ErrorFormatoArchivo":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=errorFormatoArchivo';
+                break;
+
+            case "ErrorArchivoNoValido":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=errorArchivoNoValido';
+                break;
+
+            case "ErrorCargarArchivo":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=errorCargarArchivo';
+                break;
+
+            case "ErrorNoCargaInformacionHojaCalculo":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=errorCargarInformacion';
+                break;
+
+            case "ErrorInformacionCargar":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=errorInformacionCargar';
+                $variable .= '&log=' . $valor;
+                break;
+
+            case "ExitoInformacion":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=exitoInformacion';
+                break;
+
+            case "ErrorCreacionContratos":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=errorCreacionContratos';
+                break;
+
+            case "ExitoRegistroProceso":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=exitoRegistroProceso';
+                break;
+
+            case "ErrorRegistroProceso":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=errorRegistroProceso';
+                break;
+
+            case "ErrorActualizacion":
+                $variable = 'pagina=plantillaFamiliares';
+                $variable .= '&mensajeModal=errorActualizacion';
+                break;
+        }
+        foreach ($_REQUEST as $clave => $valor) {
+            unset($_REQUEST[$clave]);
+        }
+
+        $url = $miConfigurador->configuracion["host"] . $miConfigurador->configuracion["site"] . "/index.php?";
+        $enlace = $miConfigurador->configuracion['enlace'];
+        $variable = $miConfigurador->fabricaConexiones->crypto->codificar($variable);
+        $_REQUEST[$enlace] = $enlace . '=' . $variable;
+        $redireccion = $url . $_REQUEST[$enlace];
+
+        echo "<script>location.replace('" . $redireccion . "')</script>";
+
+        exit();
+    }
+}
+?>
