3ec616a3b24a84c86b1dc3d2f7a5c9d813f354e0
violetasdev <vsosa.ud@gmail.com>
2017-02-07 14:25:20 -0500

    CRUD beneficiario rol unitario

---
+++ blocks/facturacion/beneficiarioRol/frontera/locale/es_es/Mensaje.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Etiquetas correspondientes a los Formularios
+ */
+
+$this->idioma['noDefinido'] = 'Etiqueta No definida';
+
+$this->idioma['beneficiario'] = 'Beneficiario';
+$this->idioma['rol'] = 'Rol';
+$this->idioma['botonCrear'] = 'Registrar Nueva Asociación';
+$this->idioma['botonCalcular'] = 'Guardar Datos';
+
+$this->idioma["eliminarCabecera"]="¿Realmente desea eliminar el asociación?";
+$this->idioma["cabeceraEliminado"]="Ha sido Eliminada el asociación";
+$this->idioma["cabeceraNoEliminado"]="No Ha sido posible eliminar el asociación";
+
+$this->idioma["botonAceptarElim"]="Eliminar";
+$this->idioma["botonCancelarElim"]="Cancelar";
+$this->idioma["botonCerrar"]="Aceptar";
+$this->idioma["botonCerrar2"]="Aceptar";
+?>
