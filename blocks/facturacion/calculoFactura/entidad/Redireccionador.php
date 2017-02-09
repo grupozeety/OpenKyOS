6694cbbcc83c2e4d2b1d8813da14cc740f905c17
violetasdev <vsosa.ud@gmail.com>
2017-02-09 09:30:31 -0500

    redirección cálculo factura

--- blocks/facturacion/calculoFactura/entidad/Redireccionador.php
+++ blocks/facturacion/calculoFactura/entidad/Redireccionador.php
@@ -10,63 +10,21 @@ class Redireccionador {
 	public static function redireccionar($opcion, $valor = "") {
 		$miConfigurador = \Configurador::singleton ();
 
+
 		switch ($opcion) {
-			
-			case "ErrorFormatoArchivo" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorFormatoArchivo';
-				break;
-			
-			case "ErrorArchivoNoValido" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorArchivoNoValido';
-				break;
-			
-			case "ErrorCargarArchivo" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorCargarArchivo';
-				break;
-			
-			case "ErrorNoCargaInformacionHojaCalculo" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorCargarInformacion';
-				break;
-			
-			case "ErrorInformacionCargar" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorInformacionCargar';
-				$variable .= '&log=' . $valor;
-				break;
 			
 			case "ExitoInformacion" :
 				$variable = 'pagina=calculoFactura';
 				$variable .= '&mensajeModal=exitoInformacion';
 				break;
 			
-			case "ErrorCreacionContratos" :
+			case "ErrorInformacion" :
 				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorCreacionContratos';
+				$variable .= '&mensajeModal=errorInformacion';
+				$variable .= '&errores='.$valor;
 				break;
 			
-			case "ExitoRegistroProceso" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=exitoRegistroProceso';
-				break;
-			
-			case "ErrorRegistroProceso" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorRegistroProceso';
-				break;
-			
-			case "ErrorActualizacion" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorActualizacion';
-				break;
-			
-			case "ErrorActualizacioncabecera" :
-				$variable = 'pagina=calculoFactura';
-				$variable .= '&mensajeModal=errorActualizacionCab';
-				break;
+		
 		}
 		foreach ( $_REQUEST as $clave => $valor ) {
 			unset ( $_REQUEST [$clave] );
