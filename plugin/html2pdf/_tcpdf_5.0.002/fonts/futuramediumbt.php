0259fbe5d2e8f2974949a323327fdb8888bf799c
stiv15 <stivmarquez.ud@gmail.com>
2017-05-24 19:20:58 -0500

    Fuentes Correpondientes a la Impresion de la Factura

---
+++ plugin/html2pdf/_tcpdf_5.0.002/fonts/futuramediumbt.php
@@ -0,0 +1,15 @@
+<?php
+// TCPDF FONT FILE DESCRIPTION
+$type='TrueTypeUnicode';
+$name='FuturaBT-Medium';
+$up=-109;
+$ut=71;
+$dw=963;
+$diff='';
+$originalsize=45919;
+$enc='';
+$file='futuramediumbt.z';
+$ctg='futuramediumbt.ctg.z';
+$desc=array('Flags'=>32,'FontBBox'=>'[-167 -236 1141 963]','ItalicAngle'=>0,'Ascent'=>963,'Descent'=>-236,'Leading'=>0,'CapHeight'=>963,'XHeight'=>963,'StemV'=>34,'StemH'=>15,'AvgWidth'=>458,'MaxWidth'=>1169,'MissingWidth'=>963);
+$cw=array(0=>963);
+// --- EOF ---
