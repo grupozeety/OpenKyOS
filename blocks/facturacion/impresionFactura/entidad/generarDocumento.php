6ea46fbe001c3c9098f8178eb7eed930ba495389
stiv15 <stivmarquez.ud@gmail.com>
2017-06-28 16:47:32 -0500

    Ajuste Logo de Forma de Pago de Sucre

--- blocks/facturacion/impresionFactura/entidad/generarDocumento.php
+++ blocks/facturacion/impresionFactura/entidad/generarDocumento.php
@@ -925,7 +925,8 @@ class GenerarDocumento
 
                     case '70':
 
-                        $this->contenido .= "<td style='height:13px;text-align:left;border:none;width:70%;'><br>Administración del conjunto residencial<br><br>Horario: Lunes a Viernes de 8:00 a 12:00 y de 2:00 a 6:00 pm</td>";
+                        $this->contenido .= "<td style='height:13px;text-align:left;border:none;width:70%;'><img width='200' height='50' src='http://localhost/OpenKyOS/theme/basico/img/FormaPago2.1.png'></td>";
+//                        $this->contenido .= "<td style='height:13px;text-align:left;border:none;width:70%;'><br>Administración del conjunto residencial<br><br>Horario: Lunes a Viernes de 8:00 a 12:00 y de 2:00 a 6:00 pm</td>";
                         break;
 
                     default:
